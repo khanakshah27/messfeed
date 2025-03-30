dbbdaf72a520a4aed6640c53cabc3ab5714e47b5
khanakshah27 <167800358+khanakshah27@users.noreply.github.com>
2025-03-30 12:00:11 +0530

    Create dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "messfeed_db";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
