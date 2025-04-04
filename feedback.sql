7643d2f749c22594cc190a26c9c02672a3bff27b
khanakshah27 <167800358+khanakshah27@users.noreply.github.com>
2025-04-04 15:02:21 +0530

    Create feedback.sql

---
+++ feedback.sql
@@ -0,0 +1,7 @@
+CREATE TABLE feedback (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    feedback TEXT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
